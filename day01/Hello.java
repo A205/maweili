c8bec1301e5bafb7813698bd400c996d695c0c5a
maweili <1251118764@qq.com>
2015-07-08 19:45:00 +0800

    Revert "homework"
    This reverts commit 3f2d549756f6d21d8b67df86a97e3a583f16b586.

--- day01/Hello.java
+++
@@ -1,9 +0,0 @@
-import java.util.Scanner;
-
-public class Hello {
-	public static void main(String[] args){
-		Scanner scanner=new Scanner(System.in);
-                String ch=scanner.next();
-                System.out.println("Hello "+ch);
-  }
-}
